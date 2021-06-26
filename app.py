7a41d2bceb635412d677507653b1cdcaa66ca9f2
marc1730 <marcus713@protonmail.com>
2021-06-26 14:34:56 +0800

    Fixed tensorflow 2.4.0 syntax error

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 from numpy import loadtxt
-from keras.models import load_model
+import tensorflow as tf
+from tensorflow.keras.models import load_model
 import pandas as pd
 import numpy as np
 import pickle
